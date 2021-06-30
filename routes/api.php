8059dc54b6bc5112e41944ebfde3438b2be9e515
@Ardyan172 <ardyanrhm321@gmail.com>
2021-06-30 12:21:53 +0700

    memodifikasi route/api.php karena saya ingin mengganti semua rute manual menjadi rute resource

--- routes/api.php
+++ routes/api.php
@@ -8,14 +8,18 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/transaction', [TransactionController::class, 'index']);
-// menampilkan data dari table
-Route::post('/transaction/store', [TransactionController::class, 'store']);
-// untuk menambahkan data
-Route::get('/transaction/show/{id}', [TransactionController::class, 'show']);
-// menampilkan data berdasarkan id
-Route::get('/transaction/destroy/{id}', [TransactionController::class, 'destroy']);
-// menghapus data berdasarkan id
-Route::put('/transaction/update/{id}', [TransactionController::class, 'update']);
+// Route::get('/transaction', [TransactionController::class, 'index']);
+// // menampilkan data dari table
+// Route::post('/transaction/store', [TransactionController::class, 'store']);
+// // untuk menambahkan data
+// Route::get('/transaction/show/{id}', [TransactionController::class, 'show']);
+// // menampilkan data berdasarkan id
+// Route::get('/transaction/destroy/{id}', [TransactionController::class, 'destroy']);
+// // menghapus data berdasarkan id
+// Route::put('/transaction/update/{id}', [TransactionController::class, 'update']);
 
+// rute resource berguna untuk melakukan semua fitur CRUD, jadi tidak perlu menulis rute manual seperti diatas
+// tapi nama method di transactionController jangan diubah karena nanti error
+Route::resource('/transaction', TransactionController::class)->except('create', 'edit');
+// except di gunakan agar url dan method create dan edit tidak bisa di akses
 
